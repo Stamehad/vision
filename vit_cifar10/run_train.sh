26e84065f8a3038d56417c906e351c07c4bd47bb
Stamehad <itamarshamir@gmail.com>
2025-02-23 11:06:53 +0200

    small change

--- vit_cifar10/run_train.sh
+++ vit_cifar10/run_train.sh
@@ -4,7 +4,7 @@
 source ~/miniconda3/bin/activate my_vision
 
 # Run training
-python train.py --config cifar_config.yaml --epochs 4 # --checkpoint /Users/itamarshamir/Documents/code/Vision/vit_cifar10/lightning_logs/ViT_CIFAR10/version_1/checkpoints/epoch=0-step=391.ckpt # --batch_size 64 --lr 0.001 --num_workers 8
+python train.py --config cifar_config.yaml --epochs 8 --checkpoint /vit_cifar10/lightning_logs/ViT_CIFAR10/version_10/checkpoints/epoch=3-step=1564.ckpt # --batch_size 64 --lr 0.001 --num_workers 8
 
 # Start TensorBoard
 tensorboard --logdir "lightning_logs" --port 6006
